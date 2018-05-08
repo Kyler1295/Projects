485c6b8a530002a63ad5c25d416565bc3e5c9e31
Kyle Rosen <kyler1295@gmail.com>
2018-05-08 15:58:31 -0400

    Large Updates : API, Vue, Code.

---
+++ RiotGamesProject/scripts/RiotGamesApi.js
@@ -0,0 +1,95 @@
+    class Champion {
+        constructor(champKey, name, armor, armorpl, ad, adpl, attrng, attspdpl, hp, hppl, hpregen, hpregenpl, ms, mana, manapl, manareg, manaregpl, mr, mrpl, spells, show) {
+            this.champKey = champKey;
+            this.name = name;
+            this.armor = armor;
+            this.armorpl = armorpl;
+            this.ad = ad;
+            this.adpl = adpl;
+            this.attrng = attrng;
+            this.attspdpl = attspdpl;
+            this.hp = hp;
+            this.hppl = hppl;
+            this.hpregen = hpregen;
+            this.hpregenpl = hpregenpl;
+            this.ms = ms;
+            this.mana = mana;
+            this.manapl = manapl;
+            this.manareg = manareg;
+            this.manaregpl = manaregpl;
+            this.mr = mr;
+            this.mrpl = mrpl;
+            this.spells = spells;
+            this.show = false;
+        }
+    }
+
+    const app = new Vue({
+        el: '#app',
+        data: {
+            championSearch: '',
+            champions: []
+        },
+        mounted() {
+            fetch('RiotAPIResponse.json')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    return Object.entries(data)
+                })
+                .then((jsonResponse) => {
+                    Object.entries(jsonResponse[2][1]).forEach(element => {
+                        let newChampion = new Champion(
+                            element[1].key,
+                            element[1].name,
+                            element[1].stats.armor,
+                            element[1].stats.armorperlevel,
+                            element[1].stats.attackdamage,
+                            element[1].stats.attackdamageperlevel,
+                            element[1].stats.attackrange,
+                            element[1].stats.attackspeedperlevel,
+                            element[1].stats.hp,
+                            element[1].stats.hpperlevel,
+                            element[1].stats.hpregen,
+                            element[1].stats.hpregenperlevel,
+                            element[1].stats.movespeed,
+                            element[1].stats.mp,
+                            element[1].stats.mpperlevel,
+                            element[1].stats.mpregen,
+                            element[1].stats.mpregenperlevel,
+                            element[1].stats.spellblock,
+                            element[1].stats.spellblockperlevel,
+                            element[1].spells
+                        )
+
+                        this.champions.push(newChampion);
+
+                    });
+                }).then(() => {
+                    this.champions.sort(function (a, b) {
+                        if (a.name < b.name) return -1;
+                        if (a.name > b.name) return 1;
+                        return 0;
+                    });
+                })
+        },
+        methods: {
+            closeAll(championName) {
+                this.champions.forEach(champion => {
+                    if(championName == champion.name && champion.show != true){
+                        champion.show = true
+                    } else {
+                        champion.show = false
+                    }
+                });
+            }
+        },
+        computed: {
+            championSearchResults() {
+                return this.champions.filter((champion) => {
+                    return champion.name.toLowerCase().includes(this.championSearch.toLowerCase());
+                });
+            }
+        }
+    });
